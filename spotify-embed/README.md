10ad210e81e6cc611b1b3d982a3c7f8885101298
wtzplay <welson@wtzplay.com>
2025-04-12 04:47:23 +0800

    back button refinement

---
+++ spotify-embed/README.md
@@ -0,0 +1,5 @@
+# Spotify Embed testing in PWA for Android
+
+Documentation visit https://docs.google.com/document/d/slash/1JspvXtMweFHFLBHAE8CKGLZCO
+
+remove both slash and / at center, and then append it with `noBPlXzTNEMu2js5gE`
---
+++ spotify-embed/main.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function() {
+  window.history.pushState({ noBackExitsApp: true }, '')
+})
+
+window.addEventListener('popstate', function(event) {
+  if (event.state && event.state.noBackExitsApp) {
+    window.history.pushState({ noBackExitsApp: true }, '')
+  }
+})
