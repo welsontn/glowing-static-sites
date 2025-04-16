7e7a5341d0846a14d90629dc1ea9dd2500d7aa40
wtzplay <welson@wtzplay.com>
2025-04-17 01:44:18 +0800

    Landscape lock test

--- spotify-embed/main.js
+++ spotify-embed/main.js
@@ -7,3 +7,12 @@ window.addEventListener('popstate', function(event) {
     window.history.pushState({ noBackExitsApp: true }, '')
   }
 })
+
+
+// Orientation Test
+screen.orientation.lock("landscape-primary")
+screen.orientation.addEventListener("change", (event) => {
+  const type = event.target.type;
+  const angle = event.target.angle;
+  console.log(`ScreenOrientation change: ${type}, ${angle} degrees.`);
+});
